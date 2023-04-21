ecf938cd2573a0375b0fef6b3df273b167d871fe
DanielOrres <118545121+DanielOrres@users.noreply.github.com>
2023-04-21 19:14:06 +0200

    UI BASICA
    HECHO

---
+++ AR_proyecto_final/Assets/Programacion/Torres/Scripts/Botones/VolverMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolverMenu : MonoBehaviour
+{
+    public void CambiarEscena()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+
+
+
+
+   
+}
---
+++ AR_proyecto_final/Assets/Programacion/Torres/Scripts/Botones/play.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class play : MonoBehaviour
+{
+   
+    public void IrJuego()
+    {
+        SceneManager.LoadScene("Prueba_D");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
