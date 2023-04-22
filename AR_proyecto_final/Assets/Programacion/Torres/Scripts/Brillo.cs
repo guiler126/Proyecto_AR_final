48195def125a90cf9ca97d00e592be7f4ffd4857
DanielOrres <118545121+DanielOrres@users.noreply.github.com>
2023-04-22 15:22:39 +0200

    Brillo
    SliderBrillo

--- AR_proyecto_final/Assets/Programacion/Torres/Scripts/Brillo.cs
+++ AR_proyecto_final/Assets/Programacion/Torres/Scripts/Brillo.cs
@@ -5,19 +5,27 @@ using UnityEngine.UI;
 
 public class Brillo : MonoBehaviour
 {
-
     //Variables publicas
-    
+    public Slider slider;
+    public float sliderValue;
+    public Image panelBrillo;
     
 
 
     void Start()
     {
+
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
         
     }
 
-    void Update()
+   
+    public void CambiarSider(float valor)
     {
-        
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
     }
 }
