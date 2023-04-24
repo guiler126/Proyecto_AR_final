1d42f69d0015db719131939e73a73df16c76fa76
guiler126 <128089192+guiler126@users.noreply.github.com>
2023-04-24 20:58:36 +0200

    animaciones enemigo

--- AR_proyecto_final/Assets/Programacion/Guilem N/scripts/Arma.cs
+++ AR_proyecto_final/Assets/Programacion/Guilem N/scripts/Arma.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,13 +7,19 @@ public class Arma : MonoBehaviour
 {
     public GameObject player;
     public GameObject respawn;
+    public Animator Animator;
+
+    
+
     public void OnTriggerEnter(Collider other)
     {
+        Animator.SetBool("Atacar", true);
         Debug.Log("he entrado en el trigger");
         if (other.CompareTag("Player"))
         {
             Debug.Log("Has muerto");
             SceneManager.LoadScene("pruevaGuillem");
+            Animator.SetBool("Atacar", false);
         }
     }
 }
