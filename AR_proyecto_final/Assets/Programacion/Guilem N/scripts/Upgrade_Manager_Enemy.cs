864c6c34fe8f1e0a5e6a0c0aa729b320a62d10fa
alexmagano <118544879+alexmagano@users.noreply.github.com>
2023-05-11 20:57:44 +0200

    [STATSENEMY]

--- AR_proyecto_final/Assets/Programacion/Guilem N/scripts/Upgrade_Manager_Enemy.cs
+++ AR_proyecto_final/Assets/Programacion/Guilem N/scripts/Upgrade_Manager_Enemy.cs
@@ -4,21 +4,62 @@ using UnityEngine;
 
 public class Upgrade_Manager_Enemy : MonoBehaviour
 {
+    public StatsInfo statsInfo_speed_ENEMY;
+    public StatsInfo statsInfo_DMG_ENEMY;
+    public StatsInfo statsInfo_hEALTH_ENEMY;
+    
+    
+    
+    private void OnEnable()
+    {
+        statsInfo_speed_ENEMY.current_lvl = 0;
+        statsInfo_DMG_ENEMY.current_lvl = 0;
+        statsInfo_hEALTH_ENEMY.current_lvl = 0;
+       
+    }
+    
+    private bool CanUpdate(StatsInfo statsInfo)
+    {
+        if (statsInfo.current_lvl +1 != statsInfo.options_list_lvl.Count )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private float ReturnStatValue(StatsInfo statsInfo)
+    {
+        statsInfo.current_lvl++;
+        return statsInfo.options_list_lvl[statsInfo.current_lvl];
+
+      
+    }
+    
     public void Upgrade_Speed()
     {
-        enemigo.Instance.navMeshAgent.speed += 0.5f;
+        if (CanUpdate(statsInfo_speed_ENEMY))
+        {
+            enemigo.Instance.navMeshAgent.speed = ReturnStatValue(statsInfo_speed_ENEMY);
+        } 
     }
 
 
     public void Upgrade_Damage()
     {
-        
+        if (CanUpdate(statsInfo_DMG_ENEMY))
+        {
+            Arma.Instance.damage_enemy = (int) ReturnStatValue(statsInfo_DMG_ENEMY);
+        } 
     }
   
   
     public void Upgrade_Health()
     {
-        BasicEnemy.Instance.health += 100;
+        if (CanUpdate(statsInfo_hEALTH_ENEMY))
+        {
+            BasicEnemy.Instance.health = (int) ReturnStatValue(statsInfo_hEALTH_ENEMY);
+        } 
     }
    
   
