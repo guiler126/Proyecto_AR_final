c202ca0676d400fc80cdb416afa2226ed1450553
alexmagano <118544879+alexmagano@users.noreply.github.com>
2023-05-12 17:14:08 +0200

    [dash]

--- AR_proyecto_final/Assets/Magano/Scripts/PlayerController.cs
+++ AR_proyecto_final/Assets/Magano/Scripts/PlayerController.cs
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float sphereRadius;
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool atackkRight;
-    [SerializeField] private bool canDash;
+    [SerializeField] public bool canDash;
     
     
     
