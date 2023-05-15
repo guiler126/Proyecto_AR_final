fb8ecf4eaee88f77b64030b73c0a7b4b0a70b04e
DanielOrres <118545121+DanielOrres@users.noreply.github.com>
2023-05-15 20:43:46 +0200

    Update idiomas
    Se han añadido nuevos elementos en la tabla de idiomas

---
+++ AR_proyecto_final/Assets/Programacion/Torres/Scripts/Botones/MostrarPanel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MostrarPanel : MonoBehaviour
+{
+
+
+    public GameObject panel;
+
+
+
+   public void Mostrarpanel()
+    {
+        if(panel != null)
+        {
+            panel.SetActive(true);
+        }
+        else
+        {
+            panel.SetActive(false);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+}
