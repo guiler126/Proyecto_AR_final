26ca03a68ca89980fff4e17676e3d6289efb054d
Aarchyy89 <abarrios@santmarc.com>
2023-05-15 16:14:08 +0200

    Boss_Spawn + fixes
    cositas

--- AR_proyecto_final/Assets/Aaron/Scripts/Boss/Boss_SPAWN.cs
+++ AR_proyecto_final/Assets/Aaron/Scripts/Boss/Boss_SPAWN.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,51 @@ using UnityEngine.UI;
 
 public class Boss_SPAWN : MonoBehaviour
 {
-
+    [Header("---GO--")]
     public GameObject Boss;
-    public bool Activo;
     public GameObject Boss_Panel;
-    //public Animator ANIM;
+    public GameObject portal;
+    public Material Night;
+
+    [SerializeField] private float spawn_timer = 0f;
+    [SerializeField] private float time_to_spawn = 4.5f;
+    [SerializeField] private float portal_timer = 5f;
+    [SerializeField] private float timer_out = 5f;
+    public bool Activo;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
-            Activo = true;
+            Portal_Boss();
+            
+        }
+        if (Activo == true)
+        {
+            spawn_timer += Time.deltaTime;
+        }
+        if (spawn_timer >= time_to_spawn)
+        {
             Boss.SetActive(true);
-            Boss_Panel.SetActive(true); 
-            //GetComponent<Animator>().SetBool("EstoyCorriendo", true);
+            Boss_Panel.SetActive(true);
+            
+        }
 
+        if(spawn_timer >= timer_out)
+        {
+            portal.SetActive(false);
+            Activo = false;
+            spawn_timer = 0;
         }
 
     }
 
-   
+    private void Portal_Boss()
+    {
+
+        Activo = true;
+        portal.SetActive(true);
+        RenderSettings.skybox = Night;
+
+    }
 }
