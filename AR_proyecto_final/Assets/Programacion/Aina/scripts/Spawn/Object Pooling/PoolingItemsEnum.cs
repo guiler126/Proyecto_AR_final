f9c86acee3eee7af9191d5cb187355cab51e4529
alexmagano <118544879+alexmagano@users.noreply.github.com>
2023-05-19 20:59:51 +0200

    negratas de mierda

--- AR_proyecto_final/Assets/Programacion/Aina/scripts/Spawn/Object Pooling/PoolingItemsEnum.cs
+++ AR_proyecto_final/Assets/Programacion/Aina/scripts/Spawn/Object Pooling/PoolingItemsEnum.cs
@@ -2,9 +2,9 @@ public enum PoolingItemsEnum
 {
         Enemy_basic,
         Enemy_boss,
-        FVX_attack1,
-        FVX_attack2,
-        FVX_dash,
+        FVX_attack1_hielo,
+        FVX_attack2_fuego,
+        FVX_secundary,
         FVX_boss_slash,
         FVX_boss_loaded_slash,
         FVX_spawnenemy,
