9c7bd753f4388121bdb18be050b1993e92b53489
DanielOrres <118545121+DanielOrres@users.noreply.github.com>
2023-05-24 15:13:24 +0200

    Cambios en lógica brillo

--- AR_proyecto_final/Assets/Programacion/Torres/Scripts/Brillo.cs
+++ AR_proyecto_final/Assets/Programacion/Torres/Scripts/Brillo.cs
@@ -9,6 +9,11 @@ public class Brillo : MonoBehaviour
     public Slider slider;
     public float sliderValue;
     public Image panelBrillo;
+    public Image panelBrillo2;
+    public Image panelBrillo3;
+    public Image panelBrillo4;
+    public Image panelBrillo5;
+    public Image panelBrillo6;
     
 
 
@@ -16,8 +21,18 @@ public class Brillo : MonoBehaviour
     {
 
         slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
+        slider.value = PlayerPrefs.GetFloat("brillo", 0.5f);
 
         panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
+        panelBrillo2.color = new Color(panelBrillo2.color.r, panelBrillo2.color.g, panelBrillo2.color.b, slider.value);
+        panelBrillo3.color = new Color(panelBrillo3.color.r, panelBrillo3.color.g, panelBrillo3.color.b, slider.value);
+        panelBrillo4.color = new Color(panelBrillo4.color.r, panelBrillo4.color.g, panelBrillo4.color.b, slider.value);
+        panelBrillo5.color = new Color(panelBrillo5.color.r, panelBrillo5.color.g, panelBrillo5.color.b, slider.value);
+        panelBrillo6.color = new Color(panelBrillo6.color.r, panelBrillo6.color.g, panelBrillo6.color.b, slider.value);
         
     }
 
@@ -25,7 +40,22 @@ public class Brillo : MonoBehaviour
     public void CambiarSider(float valor)
     {
         sliderValue = valor;
+        sliderValue = valor;
+        sliderValue = valor;
+        sliderValue = valor;
+        sliderValue = valor;
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        PlayerPrefs.SetFloat("brillo", sliderValue);
+        PlayerPrefs.SetFloat("brillo", sliderValue);
         PlayerPrefs.SetFloat("brillo", sliderValue);
         panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, slider.value);
+        panelBrillo2.color = new Color(panelBrillo2.color.r, panelBrillo2.color.g, panelBrillo2.color.b, slider.value);
+        panelBrillo3.color = new Color(panelBrillo3.color.r, panelBrillo3.color.g, panelBrillo3.color.b, slider.value);
+        panelBrillo4.color = new Color(panelBrillo4.color.r, panelBrillo4.color.g, panelBrillo4.color.b, slider.value);
+        panelBrillo5.color = new Color(panelBrillo5.color.r, panelBrillo5.color.g, panelBrillo5.color.b, slider.value);
+        panelBrillo6.color = new Color(panelBrillo6.color.r, panelBrillo6.color.g, panelBrillo6.color.b, slider.value);
     }
 }
