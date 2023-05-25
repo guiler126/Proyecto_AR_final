dc597e16dfff61846c54d1c9a2a9ca5672b66eab
Aina <a.nees@santmarc.com>
2023-05-25 18:12:35 +0200

    Upgrade UI OK

--- AR_proyecto_final/Assets/Magano/Scripts/Ui_Manager.cs
+++ AR_proyecto_final/Assets/Magano/Scripts/Ui_Manager.cs
@@ -20,6 +20,12 @@ public class Ui_Manager : MonoBehaviour
 
     public PoolingItemsEnum enemy;
 
+    [Header("----- Mejora List -----")]
+    public List<StatsInfo> _statsInfoPlaye;
+    public List<StatsInfo> _randomStatsInfos;
+    [SerializeField] private GameObject content_mejora_list;
+    [SerializeField] private Item_Mejora item_mejora_list;
+
     private void Awake()
     {
         if (instance == null)
@@ -32,9 +38,19 @@ public class Ui_Manager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        Refresh_Mejora_List();
+    }
+
     private void Update()
     {
-        Update_Health();
+        //Update_Health();
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Refresh_Mejora_List();
+        }
     }
 
     public void Update_Health()
@@ -82,5 +98,68 @@ public class Ui_Manager : MonoBehaviour
         PoolingManager.Instance.DesactivatePooledObject((int)enemy);
         Sistema_Oleadas.Instance.Checker();
         Sistema_Missions.instance.StartMissionRound();
+    }
+
+    public void Refresh_Mejora_List()
+    {
+        Clean_Sound_List();
+
+        _randomStatsInfos = new List<StatsInfo>();
+        _randomStatsInfos = GetRandomValuesFromList(_statsInfoPlaye, 3);
+
+        foreach (var mejora in _randomStatsInfos)
+        {
+            Item_Mejora _item_mejora_list;
+            _item_mejora_list = Instantiate(item_mejora_list, content_mejora_list.transform);
+            _item_mejora_list.title = mejora.title;
+            _item_mejora_list.description = mejora.description;
+            _item_mejora_list.icon.sprite = mejora.icon;
+            _item_mejora_list.background.sprite = mejora.background;
+            _item_mejora_list.name = mejora.name;
+            _item_mejora_list._statsInfo = mejora;
+        }
+    }    
+
+    private void Clean_Sound_List()
+    {
+        foreach (Transform child in content_mejora_list.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+    
+    
+    private List<StatsInfo> GetRandomValuesFromList(List<StatsInfo> list, int count)
+    {
+        List<StatsInfo> randomValues = new List<StatsInfo>();
+        System.Random random = new System.Random();
+        int statsNoMaxLvl = 0;
+        
+        foreach (var value in list)
+        {
+            if (!value.maxLvl)
+            {
+                ++statsNoMaxLvl;
+            }
+        }
+
+        if (statsNoMaxLvl < count)
+        {
+            count = statsNoMaxLvl;
+        }
+        
+        // Obtener índices aleatorios hasta obtener la cantidad deseada
+        while (randomValues.Count < count)
+        {
+            int randomIndex = random.Next(0, list.Count);
+
+            // Asegurarse de que el valor no se repita
+            if (!randomValues.Contains(list[randomIndex]) && !list[randomIndex].maxLvl)
+            {
+                randomValues.Add(list[randomIndex]);
+            }
+        }
+
+        return randomValues;
     }
 }
---
+++ AR_proyecto_final/Assets/Programacion/Guilem N/scripts/Item_Mejora.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.UI;
+
+public class Item_Mejora : MonoBehaviour
+{
+    public LocalizedString title;
+    
+    public LocalizedString description;
+
+    public Image icon;
+    
+    public Image background;
+
+    public GameObject content;
+    
+    public GameObject star;
+
+    public StatsInfo _statsInfo;
+
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Mejora);
+
+        if (_statsInfo != null)
+        {
+            for (int i = 0; i < _statsInfo.current_lvl + 1; i++)
+            {
+                Instantiate(star, content.transform);
+            }
+        }
+    }
+
+    private void Mejora()
+    {
+        if (GameManager.instance.PointStats > 0)
+        {
+            --GameManager.instance.PointStats;
+            _statsInfo.current_lvl++;
+            Upgrade_Manager_Player.instace.RefreshAllStats();
+            gameObject.SetActive(false);
+        }
+    }
+}
