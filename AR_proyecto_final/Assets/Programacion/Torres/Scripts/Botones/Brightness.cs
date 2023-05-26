9ed7bc30dd66b78243b1885d6e185a060af30685
sbaz94collect <114689345+sbaz94collect@users.noreply.github.com>
2023-05-26 06:08:42 +0200

    tablaIdiomas

--- AR_proyecto_final/Assets/Programacion/Torres/Scripts/Botones/Brightness.cs
+++ AR_proyecto_final/Assets/Programacion/Torres/Scripts/Botones/Brightness.cs
@@ -12,12 +12,13 @@ public class Brightness : MonoBehaviour
     public Volume currentVolume;
     public Volume uiVolume; // Nuevo volumen para la UI
     public ColorAdjustments colorAdjustments;
+    public ColorAdjustments uiColorAdjustments; // Nuevo componente ColorAdjustments para la UI
     private VolumeParameter<float> postExpo = new VolumeParameter<float>();
 
     private void Start()
     {
         currentVolume.profile.TryGet<ColorAdjustments>(out colorAdjustments);
-        
+        //uiVolume.profile.TryGet<ColorAdjustments>(out uiColorAdjustments); // Obtener el componente ColorAdjustments de la UI
 
         float savedValue = PlayerPrefs.GetFloat("BrightnessValue", 1f);
 
@@ -31,14 +32,19 @@ public class Brightness : MonoBehaviour
         {
             postExpo.value = value;
             colorAdjustments.postExposure.SetValue(postExpo);
-             
+
+            // Aplicar el valor de brillo también a la interfaz de usuario
+            //uiColorAdjustments.postExposure.SetValue(postExpo);
         }
         else
         {
             postExpo.value = -1.25f;
             colorAdjustments.postExposure.SetValue(postExpo);
-            
+
+            // Aplicar el valor de brillo también a la interfaz de usuario
+            //uiColorAdjustments.postExposure.SetValue(postExpo);
         }
+
         PlayerPrefs.SetFloat("BrightnessValue", value);
         PlayerPrefs.Save();
     }
